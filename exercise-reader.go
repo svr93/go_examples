086cf8879c9aa9cdab53ca72be9b56895d59f61a
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir-2.local>
2016-01-03 22:55:40 +0300

    add_exercise-reader.go

---
+++ exercise-reader.go
@@ -0,0 +1,26 @@
+/*
+ * https://tour.golang.org/methods/11
+ * 
+ * Implement a Reader type that emits an infinite stream
+ * of the ASCII character 'A'
+ */
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (myReader MyReader) Read(b []byte) (int, error) {
+
+    for i := range b {
+
+        b[i] = 'A'
+    }
+    
+    return len(b), nil
+}
+
+func main() {
+
+    reader.Validate(MyReader{})
+}
