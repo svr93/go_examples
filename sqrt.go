1745a57519bd9b4cbdc12e539e4fae8abfc5f557
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir-2.local>
2016-01-03 21:08:36 +0300

    add_error_to_return_values

--- sqrt.go
+++ sqrt.go
@@ -11,7 +11,9 @@ func Abs(x float64) float64 {
     if x > 0 { return x } else { return -x }
 }
 
-func Sqrt(x float64) float64 {
+func Sqrt(x float64) (float64, error) {
+
+    if (x < 0) { return 0, ErrNegativeSqrt(x) }
     
     z := 1.0
     prev := 0.0
@@ -20,9 +22,17 @@ func Sqrt(x float64) float64 {
         prev = z
         z = z - (z * z - x) / (2 * z)
     }
-    return z
+    return z, nil
+}
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+
+    return "cannot Sqrt negative number: " + fmt.Sprint(float64(e))
 }
 
 func main() {
     fmt.Println(Sqrt(2))
+    fmt.Println(Sqrt(-2))
 }
